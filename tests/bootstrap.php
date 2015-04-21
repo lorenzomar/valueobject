5b421b7785a7fbd9bce6fffdbba13583f5920513
Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
2015-04-21 17:16:02 +0200

    Add phpunit.xml.dist and tests/bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * This file is part of the ValueObjects package.
+ *
+ * (c) Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ */
+
+$loader = require __DIR__ . "/../vendor/autoload.php";
