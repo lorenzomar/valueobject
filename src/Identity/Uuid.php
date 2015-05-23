308a2175787fe085f3ad3cc1c0534758105a919d
Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
2015-05-23 14:33:49 +0200

    Add Uuid implementation

---
+++ src/Identity/Uuid.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the ValueObject package.
+ *
+ * (c) Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ */
+
+namespace ValueObject\Identity;
+
+use \Ramsey\Uuid\Uuid as BaseUuid;
+
+/**
+ * Class Uuid.
+ *
+ * @package ValueObject
+ * @author  Lorenzo Marzullo <marzullo.lorenzo@gmail.com>
+ * @link    http://github.com/lorenzomar/valueobjects
+ */
+class Uuid extends BaseUuid
+{
+
+}
